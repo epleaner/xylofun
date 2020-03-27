f9004aae9bd3e7f83a598923bca058096bd505ed
Eli <pleanbean@gmail.com>
2020-03-27 16:44:14 +0100

    Add link to other pages

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import {Flex, Text, Box, Link} from 'rebass/styled-components';
+import {Flex, Text, Box} from 'rebass/styled-components';
 import styled from 'styled-components';
+
+import StyledGatsbyLink from '@common/StyledGatsbyLink';
 
 const StyledContainer = styled(Flex)`
   position: fixed;
-  z-index: 1;
+  z-index: 2;
+  box-shadow: 0 0 10px 0 #808080a3;
 `;
 
 const NavBar = () => (
   <StyledContainer as="header" bg="white" width={1} p={2} alignItems="center">
-    <Text fontWeight="bold">Xylofun</Text>
+    <StyledGatsbyLink to="/">
+      <Text fontWeight="bold">Xylofun</Text>
+    </StyledGatsbyLink>
     <Box mx="auto" />
-    <Link variant="nav" href="#!">
-      About
-    </Link>
+    <Box mx={3}>
+      <StyledGatsbyLink to="/about">
+        <Text>About</Text>
+      </StyledGatsbyLink>
+    </Box>
+    <Box mx={3}>
+      <StyledGatsbyLink to="/gallery">
+        <Text>Gallery</Text>
+      </StyledGatsbyLink>
+    </Box>
+    <Box mx={3}>
+      <StyledGatsbyLink to="/who-we-are">
+        <Text>Who We Are</Text>
+      </StyledGatsbyLink>
+    </Box>
   </StyledContainer>
 );
 

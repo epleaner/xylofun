d3569158bfa326fa1276e9ffe47f0b4de8f5b571
Eli <pleanbean@gmail.com>
2020-03-27 16:44:37 +0100

    Banner can support custom height

--- src/components/common/parallax/Banner.js
+++ src/components/common/parallax/Banner.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {ParallaxBanner} from 'react-scroll-parallax';
 import BannerImage from '@images/hero/banner.jpg';
 
-const Banner = ({imgSrc, children}) => (
+const Banner = ({height, imgSrc, children}) => (
   <ParallaxBanner
-    style={{height: '100vh'}}
+    style={{height: height || '100vh'}}
     layers={[
       {
         image: imgSrc || BannerImage,

193f1b6797d272e54b1c2fc7f34f91d9ea3b3fec
Eli <pleanbean@gmail.com>
2020-03-27 19:13:08 +0100

    Add masonry gallery

--- src/components/sections/Gallery.js
+++ src/components/sections/Gallery.js
@@ -1,6 +1,59 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
+import {graphql, useStaticQuery} from 'gatsby';
+import Gallery from 'react-photo-gallery';
+import Carousel, {Modal, ModalGateway} from 'react-images';
+
 import Section from '@common/Section';
 
-const Gallery = () => <Section></Section>;
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      contentfulMedia: allContentfulAsset {
+        edges {
+          node {
+            fixed {
+              ...GatsbyContentfulFixed_withWebp
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-export default Gallery;
+  const openLightbox = useCallback((event, {photo, index}) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  const {contentfulMedia} = data;
+
+  const photos = contentfulMedia.edges.map(({node: {fixed}}) => fixed);
+  debugger;
+  return (
+    <Section>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </Section>
+  );
+};

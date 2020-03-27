06f2561bac917718dd810500887bc0475e757433
Eli <pleanbean@gmail.com>
2020-03-27 16:46:07 +0100

    Add line styling

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,12 @@ import rebassPresetTheme from '@rebass/preset';
 
 const theme = {
   ...rebassPresetTheme,
-  ...{},
+  ...{
+    lineHeights: {
+      body: 1.75,
+      heading: 1.25,
+    },
+  },
 };
 
 export default theme;

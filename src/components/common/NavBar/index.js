a3dd4c6070f34086052eaf70598b8b415238cc07
Eli <eli.i.pleaner@gmail.com>
2020-04-06 12:12:53 +0200

    Add link to home

--- src/components/common/NavBar/index.js
+++ src/components/common/NavBar/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import {Flex, Text, Box} from 'rebass/styled-components';
-import styled from 'styled-components';
+import React from "react"
+import { Flex, Text, Box } from "rebass/styled-components"
+import styled from "styled-components"
 
-import StyledGatsbyLink from '@common/StyledGatsbyLink';
-import WideMenu from '@common/NavBar/WideMenu';
-import MobileMenu from '@common/NavBar/MobileMenu';
+import StyledGatsbyLink from "@common/StyledGatsbyLink"
+import WideMenu from "@common/NavBar/WideMenu"
+import MobileMenu from "@common/NavBar/MobileMenu"
+import BrandText from "@common/BrandText"
 
 const StyledContainer = styled(Flex)`
   position: fixed;
   z-index: 2;
   box-shadow: 0 0 10px 0 #808080a3;
-`;
+`
 
-const navItems = ['About', 'Gallery', 'Who We Are', 'How to Order', 'Contact'];
+const navItems = [
+  "Home",
+  "About",
+  "Gallery",
+  "Who We Are",
+  "How to Order",
+  "Contact",
+]
 
 const NavBar = () => (
   <StyledContainer
@@ -23,17 +31,17 @@ const NavBar = () => (
     px={[2, 10]}
     alignItems="center"
   >
-    <StyledGatsbyLink to="/">
-      <Text fontWeight="bold">Xylofun</Text>
+    <StyledGatsbyLink activeClassName="" to="/">
+      <BrandText fontWeight="bold" />
     </StyledGatsbyLink>
     <Box mx="auto" />
-    <Box display={['none', 'flex']}>
+    <Box display={["none", "flex"]}>
       <WideMenu items={navItems} />
     </Box>
-    <Box display={['flex', 'none']}>
+    <Box display={["flex", "none"]}>
       <MobileMenu items={navItems} />
     </Box>
   </StyledContainer>
-);
+)
 
-export default NavBar;
+export default NavBar

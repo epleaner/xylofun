7634dc35d8324328e5e37db61de09e0c460f77fe
Eli <eli.i.pleaner@gmail.com>
2020-04-20 13:36:50 +0200

    add video carousel to gallery

--- src/components/sections/Gallery.js
+++ src/components/sections/Gallery.js
@@ -1,7 +1,30 @@
-import React, { useState, useCallback } from "react"
+import React, { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Box } from "rebass/styled-components"
+import { Flex, Box } from "rebass/styled-components"
+import styled from "styled-components"
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from "react-responsive-carousel"
 import LightboxGallery from "@common/LightboxGallery"
+
+const StyledCarousel = styled(Carousel)`
+  li.slide {
+    background: none;
+  }
+  .control-prev.control-arrow::before {
+    border-right: 8px solid black;
+  }
+  .carousel.carousel-slider .control-arrow:hover {
+    background: none;
+  }
+  .control-next.control-arrow::before {
+    border-left: 8px solid black;
+  }
+  .carousel .control-dots .dot {
+    background: orange;
+    box-shadow: none;
+  }
+`
 
 import Section from "@common/Section"
 
@@ -36,6 +59,29 @@ export default () => {
   return (
     <Section>
       <Box mt={9}>
+        <StyledCarousel
+          showArrows={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+        >
+          {[
+            "409708603",
+            "409708342",
+            "409708251",
+            "409708001",
+            "409707796",
+          ].map(id => (
+            <iframe
+              key={id}
+              src={`https://player.vimeo.com/video/${id}`}
+              height="400px"
+              frameBorder="0"
+              allow="autoplay; fullscreen"
+              allowFullScreen
+            />
+          ))}
+        </StyledCarousel>
         <LightboxGallery photos={photos} />
       </Box>
     </Section>
